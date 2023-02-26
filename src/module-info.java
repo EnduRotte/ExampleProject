e75d7f47a4b4a280d91cedeb3ed1c33320bce1a7
Ashok <Ashok@DESKTOP-MHQAEMT>
2023-02-26 19:18:19 +0530

    Second Commit

--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,32 @@
-module ExampleProject {
-}
+//import org.testng.annotations.AfterMethod;
+//import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.Test;
+//
+//import com.mystore.Base.BaseClass;
+//import com.mystore.PageObjects.IndexPage;
+//
+//module ExampleProject {
+//	
+//	public class IndexPageTest extends BaseClass{
+//
+//		IndexPage indexpage;
+//		
+//		
+//		@BeforeMethod
+//		public void setup() {
+//			launchApp();
+//		}
+//		
+//		@AfterMethod
+//		public void teardown() {
+//			driver.quit();
+//		}
+//		
+//		@Test
+//		public void QuizTesting1() throws Throwable {
+//			 indexpage = new IndexPage();
+//			 indexpage.Assessment();
+//			 
+//			 
+//		}
+//}
